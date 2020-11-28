ba220223fc6a7d44332fa1b1c1f09472d8db4a69
Dmytro Chernovskyi <dimcher911@gmail.com>
2020-11-28 14:24:51 +0200

    Рефакторит код

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,10 @@
 "use strict";
 
-const categListRef = document.querySelector("#categories");
-const categRefs = categListRef.querySelectorAll(".item");
-const categAmount = categRefs.length;
-console.log(`В списке ${categAmount} категории.`);
+const categRefs = document.querySelectorAll(".item");
+console.log(`В списке ${categRefs.length} категории.`);
 
 categRefs.forEach((category) => {
-  const categoryHeaderRef = category.querySelector("h2");
-  const categoryHeaderText = categoryHeaderRef.textContent;
-  const elementAmount = category.querySelectorAll("li").length;
   console.log(
-    `Категория: ${categoryHeaderText}\nКоличество элементов: ${elementAmount}`
+    `Категория: ${category.firstElementChild.textContent}\nКоличество элементов: ${category.lastElementChild.children.length}`
   );
 });
